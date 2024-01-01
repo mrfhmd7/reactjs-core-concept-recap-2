e01fb398ab68fb8cbf512d99c6840aef16f104f1
mrfhmd7 <mrf.hmd7@gmail.com>
2024-01-01 22:34:58 +0600

    post added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 // import logo from './logo.svg';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <LoadPost></LoadPost>
       <District name="Natore" special="Kacha Golla"></District>
       <District name="Rajshahi" special="Mango"></District>
       <District name="Bogura" special="Doi"></District>
@@ -22,7 +23,7 @@ const districtStyle = {
 function District(props) {
 
   const [power, setPower] = useState(1);
-  
+
   const handlePower = () => {
     const newPower = power * 2;
     setPower(newPower);
@@ -33,6 +34,42 @@ function District(props) {
       <p>Famous for: {props.special}</p>
       <h4>Power: {power}</h4>
       <button onClick={handlePower}>Boost the power</button>
+    </div>
+  )
+}
+
+function LoadPost() {
+
+  const [posts, setPost] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/posts`)
+      .then(res => res.json())
+      .then(data => setPost(data))
+      // console.log(posts);
+  }, [])
+
+  return (
+    <div>
+      <h1>Posts: {posts.length}</h1>
+      {
+        posts.map(post =>
+          <Post
+            key={post.id}
+            body={post.body}
+            title={post.title}>
+            </Post>
+        )
+      }
+    </div>
+  )
+}
+
+function Post(props) {
+  return (
+    <div  style={districtStyle}>
+      <h3>Title: {props.title}</h3>
+      <p>post: {props.body}</p>
     </div>
   )
 }
