1a84d047a2531727b9686439f7c94c9713493dbf
mrfhmd7 <mrf.hmd7@gmail.com>
2024-01-01 22:04:09 +0600

    power added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <District name="Natore" special="Kacha Golla"></District>
+      <District name="Rajshahi" special="Mango"></District>
+      <District name="Bogura" special="Doi"></District>
     </div>
   );
+}
+
+const districtStyle = {
+  backgroundColor: 'lightBlue',
+  margin: '15px',
+  padding: '10px',
+  borderRadius: '10px',
+}
+
+function District(props) {
+
+  const [power, setPower] = useState(1);
+  
+  const handlePower = () => {
+    const newPower = power * 2;
+    setPower(newPower);
+  }
+  return (
+    <div style={districtStyle}>
+      <h2>Name: {props.name}</h2>
+      <p>Famous for: {props.special}</p>
+      <h4>Power: {power}</h4>
+      <button onClick={handlePower}>Boost the power</button>
+    </div>
+  )
 }
 
 export default App;
